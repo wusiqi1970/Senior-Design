5317fc32b5c28518dc9b02d4d1bbf7810c1c651d
wusiqi1970 <siqiwu@bu.edu>
2018-12-09 16:05:42 -0500

    some changes

--- SD/addition.php
+++ SD/addition.php
@@ -5,7 +5,7 @@ mysqli_select_db($con,'epic');
 $name = $_POST['tool_id'];
 $pass = $_POST['tool_name'];
 
-$s = "select * from tools  where Tool_Num = '$name' ";
+$s = "select * from tools where Tool_Num = '$name' ";
 
 $result = mysqli_query ($con,$s);
 
@@ -20,8 +20,8 @@ else
 {
 	$reg = "insert into tools(Tool_Num,Tool_Name) values ('$name','$pass')";
 	mysqli_query($con, $reg);
-	echo "Successfully added items";
-	//header('location:login.php');
+	//echo "Successfully added items";
+	header('location:loker.php');
 
 }
 
